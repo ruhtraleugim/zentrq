f001ad97a7b2687975d05b4910419f84ee931d5f
Arthur simões <amsbsimoes@gmail.com>
2026-04-23 11:38:42 -0300

    feat: project scaffold — Spring Boot 21, Flyway, PostgreSQL e JWT

---
+++ src/main/java/com/spectre/zentrq/ZentrqApplication.java
@@ -0,0 +1,13 @@
+package com.spectre.zentrq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZentrqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZentrqApplication.class, args);
+	}
+
+}
